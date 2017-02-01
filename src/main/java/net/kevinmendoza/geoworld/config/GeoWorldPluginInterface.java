a23474cb09d66a8c7a0c58f10ba71dd43e47f574
el-minadero <kmendoza4@ucmerced.edu>
2017-02-01 10:05:16 -0800

    s

--- src/main/java/net/kevinmendoza/geoworld/config/GeoWorldPluginInterface.java
+++ src/main/java/net/kevinmendoza/geoworld/config/GeoWorldPluginInterface.java
@@ -6,10 +6,8 @@ import net.kevinmendoza.geoworld.geology.GeologicContainer;
 import ninja.leaping.configurate.ConfigurationNode;
 
 public interface GeoWorldPluginInterface {
-
 	
 	public GeoWorldPluginInterface GetInstance();
-	public Debug getDebugger();
 	
 	public List<GeologicContainer> getGeologicContainers();
 }
--- src/main/java/net/kevinmendoza/geoworld/geology/SeedFactory.java
+++ src/main/java/net/kevinmendoza/geoworld/geology/SeedFactory.java
@@ -3,6 +3,5 @@ package net.kevinmendoza.geoworld.geology;
 public abstract class SeedFactory {
 	public static long SEED;
 	
-	
 	public abstract void setSeed(long seed);
 }
